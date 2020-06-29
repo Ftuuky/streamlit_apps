11a11d86b99eeaacdecaf8ef4ab4037a6fee216d
FM <16304630+Ftuuky@users.noreply.github.com>
2020-06-29 20:54:39 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Parisian Trees')
+
+HEIGHT_COLUMN = 'height_m'
+TYPE_COLUMN = 'tree_type'
+DATA = ("C:\\Users\\ftuuk\\Documents\\Arbres\\map.csv")
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA, nrows=nrows)
+    return data
+
+data_load_state = st.text('Loading dataset...')
+data = load_data(154626)
+data_load_state.text("Dataset is ready!")
+
+if st.checkbox('Show raw dataset'):
+    st.subheader('Raw dataset')
+    #st.dataframe(data.style.highlight_max(axis=0))
+    st.write(data)
+
+app_mode = st.selectbox("Choose a type of tree", ['Aesculus', 'Acer','Sophora', 'Prunus', 'Pyrus', 'Tilia', 'Quercus', 'Platanus', 'Pinus', 'Corylus', 'Fraxinus', 'Celtis', 'Populus', 'Other'])
+
+height_to_filter = st.slider('Select tree height', 1, 22, 10)
+filtered_data = data[(data[HEIGHT_COLUMN] == height_to_filter) & (data[TYPE_COLUMN] == app_mode)]
+
+st.subheader('Map of %s trees with height of %s meters' % (app_mode, height_to_filter))
+st.map(filtered_data)
